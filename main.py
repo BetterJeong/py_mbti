6e95b48bcc99b7899f8ac2c392dc54fb1f2f7d79
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2021-12-28 22:57:01 +0900

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import tkinter
 from tkinter import *
 from tkinter import font
@@ -293,8 +295,29 @@ def send_result() :
         isfp_frame.tkraise()
     elif (result_mbti[0] == "E" and result_mbti[1] == "S" and result_mbti[2] == "T" and result_mbti[3] == "P") :
         estp_frame.tkraise()
-    elif (result_mbti[0] == "E" and result_mbti[1] == "N" and result_mbti[2] == "T" and result_mbti[3] == "P") :
-        entp_frame.tkraise()
+    elif (result_mbti[0] == "E" and result_mbti[1] == "S" and result_mbti[2] == "T" and result_mbti[3] == "P") :
+        estp_frame.tkraise()
+#------------------------------------------------------------------------
+
+#------------------------ 첫 화면으로 돌아가는 함수 ---------------------
+def go_main() :
+    # 카운팅 모두 0으로 되돌리기
+    global cnt_E
+    global cnt_N
+    global cnt_T
+    global cnt_P
+    global result_mbti
+    cnt_E = 0
+    cnt_N = 0
+    cnt_T = 0
+    cnt_P = 0
+    result_mbti = [0, 0, 0, 0]
+    cnt_test()
+    print(result_mbti)
+    # 시작 화면으로 돌아가기
+    main_frame.tkraise()
+#------------------------------------------------------------------------
+
 #------------------------------------------------------------------------
 # font 설정
 font_main = font.Font(family = "Arial", size = 14, weight = "bold")
@@ -309,9 +332,9 @@ title_label.place(x=100, y=110)
 text_label = tkinter.Label(main_frame, text="이 테스트는 정식 검사가 아니므로\n재미로 즐겨주시기 바랍니다.\n\n자신의 모습과 제일 비슷한 답변을 골라주세요.", font = font_text)
 text_label.place(x=75, y=330)
 # 시작 버튼 배치
-start_photo = tkinter.PhotoImage(file="assets/startbutton.png")
+start_photo = tkinter.PhotoImage(file="assets/startButton.png")
 start_button = tkinter.Button(main_frame, image=start_photo, command=set_frame_q1)
-start_button.place(x=130, y=420)
+start_button.place(x=110, y=420)
 # 하단 footer 배치
 author_label = tkinter.Label(main_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
 author_label.place(x=75, y=670)
@@ -550,12 +573,305 @@ intj_label.place(x=11, y=100)
 # 타이틀 텍스트 
 title_name_text = tkinter.Label(intj_frame, text="당신의 유형은", font = font_small_title)
 title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+intj_info = tkinter.Label(intj_frame, text="무언가를 있는 그대로 받아들이기 보다는 깊게\n분석하고 이해하는 걸 좋아해요. 그리고 논리적인\n탐구 과정을 통해 결과를 이끌어내요.\n\n과정보다는 결과가 중요해요.\n자신이 생각한 이론을 실현해내는 능력이 뛰어나요.\n\n자신의 능력을 믿고 도전하는 데 능숙해요.\n아무리 어려운 목표라도 달성하려고 노력해요.\n\n감수성이 낮고, 감정 표현을 쉽게 하지 않아요.\n덕분에 주변에서 속을 알 수 없다는 말을 종종 들어요.\n\n\n✔ 학교에서 내 모습은?\n자신의 목표를 향해 최선을 다할 수 있는 멋진\n사람이에요. 친구들과 노는 것 보다는 자신의 할 일을\n더 중요하게 여기는 것 같아요. 속을 알 수 없는\n모습도 종종 보이지만, 사실 알고 보면 재밌는 친구예요.", font = font_small_title)
+intj_info.place(x=15, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button_photo = tkinter.PhotoImage(file="assets/retestButton.png")
+restest_button = tkinter.Button(intj_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
 # 하단 footer 배치
 author_label = tkinter.Label(intj_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
 author_label.place(x=75, y=670)
 # -------------------------------------------------------------------------
 
-# ESTP 결과 화면 ----------------------------------------------------------
+# intp 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(intp_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+intp_title = tkinter.PhotoImage(file="assets/intp.png")
+intp_label = tkinter.Label(intp_frame, image=intp_title)
+intp_label.place(x=35, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(intp_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+intp_info = tkinter.Label(intp_frame, text="조용하고 과묵한 성격으로, 말을 많이 하는 편이\n아니에요. 처음 만난 사람에게는 차갑게 보일 수 있어요.\n하지만 사실은 털털하고 개방적인 성격이에요.\n\n추상적이고 논리적인 생각을 하는 걸 좋아해요.\n자신이 관심있는 분야에 대해 끊임없이 사색하고\n분석해요. 비현실적인 상상도 좋아해요.\n\n규칙적이고 체계적인 건 싫어요. 계획을 짜서 움직이는\n스타일은 아니에요. 계획보다는 그때마다 상황에 따라\n즉흥적으로 하는 걸 좋아해요.\n\n\n✔ 학교에서 내 모습은?\n혼자가 좋아! 단체생활은 피곤해요. 인간관계에도\n 큰 관심이 없어요. 혼자 공부하는 게 편하고, 조용한\n도서관 분위기가 좋아요. 놀 때도 혼자 노는 게 좋아요.\n사회적인 활동을 하는 경우는 드물어요.\n도서관에서 자주 보여요.", font = font_small_title)
+intp_info.place(x=15, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(intp_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(intp_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# entj 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(entj_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+entj_title = tkinter.PhotoImage(file="assets/entj.png")
+entj_label = tkinter.Label(entj_frame, image=entj_title)
+entj_label.place(x=11, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(entj_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+entj_info = tkinter.Label(entj_frame, text="자기주장이 확고하고, 대담한 지도력과\n통솔력을 가지고 있어요.\n모임이나 팀플을 적극적으로 주도하고,\n대화를 이끌어나가는 힘이 있어요.\n\n지식을 탐구하는 데 욕심이 있어요.\n직관적인 능력이 뛰어나요.\n어떤 일을 처리할 때 깊게 분석하고\n계획하는 걸 좋아해요.\n그리고 달성을 위해 체계적인 과정을 수립해요.\n\n사회를 뒤엎고 싶어해요.\n신념에 맞지 않는 일에 적극적으로 맞서요.\n\n\n✔ 학교에서 내 모습은?\n팀플을 하면 원하지 않아도 팀장이 돼요.\n사실 내가 팀장을 하는 게 낫겠다 싶어요.\n그리고 특유의 주도적인 성격으로 팀을 잘 이끌어나가요.", font = font_small_title)
+entj_info.place(x=15, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(entj_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(entj_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# entp 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(entp_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+entp_title = tkinter.PhotoImage(file="assets/entp.png")
+entp_label = tkinter.Label(entp_frame, image=entp_title)
+entp_label.place(x=25, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(entp_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+entp_info = tkinter.Label(entp_frame, text="인간 관계에 크게 관심을 갖거나 사교적이지는 않지만,\n토론과 같은 외부 활동을 좋아해요.\n자신의 생각이나 가치관을 잘 말하는 편이에요.\n\n자기 자신에 대한 자신감이 있어요.\n그리고 다른 사람이 나에 대해 어떻게 평가하든\n 관심 없어요. 나한테 참견하는 것도 싫어요.\n 나는 내가 알아서 해요.\n\n공부에는 별로 관심 없어요. 하기 싫은 공부보다는\n내가 좋아하는 분야를 위해 공부하는 게 좋아요.\n반복적이고 꾸준히 해야 하는 일을 별로 안 좋아해요.\n\n\n✔ 학교에서 내 모습은?\n주관이 뚜렷하고 말을 잘하는 친구예요.\n토론에 능하고 논쟁을 즐겨요.\n시험에는 관심 없던 것처럼 보이더니\n벼락치기로 성적은 잘 받아요.", font = font_small_title)
+entp_info.place(x=20, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(entp_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(entp_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# infj 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(infj_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+infj_title = tkinter.PhotoImage(file="assets/infj.png")
+infj_label = tkinter.Label(infj_frame, image=infj_title)
+infj_label.place(x=25, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(infj_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+infj_info = tkinter.Label(infj_frame, text="겉으로 보기엔 차가워 보일 수도 있지만,\n사실은 매우 따뜻해요.\n다정하고 따뜻하다는 말을 많이 들어요.\n\n불의에 대해 민감하게 반응하고\n높은 도덕적 관념을 가지고 있어요.\n모든 상황을 다양한 시각으로 바라보고\n이해하는 능력이 뛰어나요.\n\n몽환적이고, 신비로운,\n어떻게 보면 가장 알 수 없는 유형이에요.\n\n호기심이 많고 열정적이에요.\n스스로에게 매우 엄격하고 자아성찰을 자주 해요.\n\n\n✔ 학교에서 내 모습은?\n종종 무슨 생각을 하는지 잘 모르겠어요.\n하지만 따스한 성격이라는 건 너무 잘 느껴져요.\n주변 고민 상담을 잘 해주고, 신경을 많이 써줘요.", font = font_small_title)
+infj_info.place(x=35, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(infj_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(infj_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# infp 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(infp_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+infp_title = tkinter.PhotoImage(file="assets/infp.png")
+infp_label = tkinter.Label(infp_frame, image=infp_title)
+infp_label.place(x=45, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(infp_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+infp_info = tkinter.Label(infp_frame, text="", font = font_small_title)
+infp_info.place(x=25, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(infp_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(infp_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# enfj 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(enfj_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+enfj_title = tkinter.PhotoImage(file="assets/enfj.png")
+enfj_label = tkinter.Label(enfj_frame, image=enfj_title)
+enfj_label.place(x=40, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(enfj_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+enfj_info = tkinter.Label(enfj_frame, text="", font = font_small_title)
+enfj_info.place(x=25, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(enfj_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(enfj_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# enfp 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(enfp_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+enfp_title = tkinter.PhotoImage(file="assets/enfp.png")
+enfp_label = tkinter.Label(enfp_frame, image=enfp_title)
+enfp_label.place(x=15, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(enfp_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+enfp_info = tkinter.Label(enfp_frame, text="", font = font_small_title)
+enfp_info.place(x=25, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(enfp_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(enfp_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# istj 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(istj_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+istj_title = tkinter.PhotoImage(file="assets/istj.png")
+istj_label = tkinter.Label(istj_frame, image=istj_title)
+istj_label.place(x=15, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(istj_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+istj_info = tkinter.Label(istj_frame, text="", font = font_small_title)
+istj_info.place(x=25, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(istj_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(istj_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# isfj 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(isfj_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+isfj_title = tkinter.PhotoImage(file="assets/isfj.png")
+isfj_label = tkinter.Label(isfj_frame, image=isfj_title)
+isfj_label.place(x=47, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(isfj_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+isfj_info = tkinter.Label(isfj_frame, text="", font = font_small_title)
+isfj_info.place(x=25, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(isfj_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(isfj_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# estj 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(estj_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+estj_title = tkinter.PhotoImage(file="assets/estj.png")
+estj_label = tkinter.Label(estj_frame, image=estj_title)
+estj_label.place(x=12, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(estj_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+estj_info = tkinter.Label(estj_frame, text="", font = font_small_title)
+estj_info.place(x=25, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(estj_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(estj_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# esfj 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(esfj_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+esfj_title = tkinter.PhotoImage(file="assets/esfj.png")
+esfj_label = tkinter.Label(esfj_frame, image=esfj_title)
+esfj_label.place(x=45, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(esfj_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+esfj_info = tkinter.Label(esfj_frame, text="", font = font_small_title)
+esfj_info.place(x=25, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(esfj_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(esfj_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# istp 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(istp_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+istp_title = tkinter.PhotoImage(file="assets/istp.png")
+istp_label = tkinter.Label(istp_frame, image=istp_title)
+istp_label.place(x=29, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(istp_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+istp_info = tkinter.Label(istp_frame, text="", font = font_small_title)
+istp_info.place(x=25, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(istp_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(istp_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# isfp 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(isfp_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+isfp_title = tkinter.PhotoImage(file="assets/isfp.png")
+isfp_label = tkinter.Label(isfp_frame, image=isfp_title)
+isfp_label.place(x=5, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(isfp_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+isfp_info = tkinter.Label(isfp_frame, text="", font = font_small_title)
+isfp_info.place(x=25, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(isfp_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(isfp_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# estp 결과 화면 ----------------------------------------------------------
 # 테스트 로고 배치
 logo_label = tkinter.Label(estp_frame, image=logo_photo)
 logo_label.place(x=160, y=10)
@@ -566,8 +882,36 @@ estp_label.place(x=0, y=100)
 # 타이틀 텍스트 
 title_name_text = tkinter.Label(estp_frame, text="당신의 유형은", font = font_small_title)
 title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+estp_info = tkinter.Label(estp_frame, text="삶을 즐기는 자유로운 성격이에요.\n신나고 스릴 넘치는 일들을 좋아해요.\n남들보다 겁이 없고, 위험한 행동을 자주 해요.\n\n미래보다는 현재가 중요해요.\n바로 지금을 즐기며 사는 게 중요하죠.\n\n무슨 일이든 중심에 서서 관심을 받는 것을 좋아해요.\n뭐든 몸으로 부딪혀 보라!\n궁금한 건 직접 경험해보아야 해요.\n\n현실적으로 생각해요.\n빠르게 결정하고 빠르게 행동하는 걸 선호해요.\n\n\n✔ 학교에서 내 모습은?\n활발하고 자유로운 인상이에요.\n하지만 공부에는 별로 관심이 없어요.\n과제할 시간에 내가 하고 싶은 일을\n하는 게 더 좋아요.", font = font_small_title)
+estp_info.place(x=25, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(estp_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
 # 하단 footer 배치
 author_label = tkinter.Label(estp_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
+author_label.place(x=75, y=670)
+# -------------------------------------------------------------------------
+
+# esfp 결과 화면 ----------------------------------------------------------
+# 테스트 로고 배치
+logo_label = tkinter.Label(esfp_frame, image=logo_photo)
+logo_label.place(x=160, y=10)
+# 유형 타이틀
+esfp_title = tkinter.PhotoImage(file="assets/esfp.png")
+esfp_label = tkinter.Label(esfp_frame, image=esfp_title)
+esfp_label.place(x=45, y=100)
+# 타이틀 텍스트 
+title_name_text = tkinter.Label(esfp_frame, text="당신의 유형은", font = font_small_title)
+title_name_text.place(x=30, y=95)
+# 유형 설명 텍스트
+esfp_info = tkinter.Label(esfp_frame, text="", font = font_small_title)
+esfp_info.place(x=25, y= 190)
+# 처음으로 돌아가는 버튼 배치
+restest_button = tkinter.Button(esfp_frame, image=restest_button_photo, command=go_main)
+restest_button.place(x=100, y=580)
+# 하단 footer 배치
+author_label = tkinter.Label(esfp_frame, text="제작: 한성대학교 IT공과대학 2171110 나은정", font = font_text)
 author_label.place(x=75, y=670)
 # -------------------------------------------------------------------------
 
